c32ff381226811f05590b7e5755b76924e2d8ea4
Feddane <chafeddane@gmail.com>
2024-04-25 17:41:19 +0100

    second commit : storing agent work

--- src/MasterServerAgent.java
+++ src/MasterServerAgent.java
@@ -4,12 +4,18 @@ import jade.lang.acl.ACLMessage;
 
 public class MasterServerAgent extends Agent {
     protected void setup() {
-        System.out.println("Master Server " + getLocalName() + " started.");
+        System.out.println("Master Server Agent " + getLocalName() + " started.");
         // Envoie une requête initiale pour démarrer l'analyse
         ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
         msg.setContent("Start analysis of forest regions");
         msg.addReceiver(new jade.core.AID("computingAgent", jade.core.AID.ISLOCALNAME));
         send(msg);
+
+        // Envoie également un message au StoringAgent
+        ACLMessage msgToStoringAgent = new ACLMessage(ACLMessage.REQUEST);
+        msgToStoringAgent.setContent("Data for storage");
+        msgToStoringAgent.addReceiver(new jade.core.AID("storingAgent", jade.core.AID.ISLOCALNAME));
+        send(msgToStoringAgent);
 
         addBehaviour(new CyclicBehaviour(this) {
             public void action() {
